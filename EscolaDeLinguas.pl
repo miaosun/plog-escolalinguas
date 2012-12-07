19ea0530e2221fd7c210e1b66e55d84efebc06aa
miaosun88 <miaosun88@de85ad67-05c1-fdaa-6c12-336baf916e5f>
2012-12-07 22:20:46 +0000

    umas alterações para o programa fica mais generica, adicionei 2 ficheiros com dados diferentes.

--- EscolaDeLinguas.pl
+++ EscolaDeLinguas.pl
@@ -23,128 +23,102 @@
 %  maximizar o lucro, reduzindo os custos operacionais
 %  admitindo que cada curso preenchera as vagas disponiveis (15/curso)
 
-% Income: 10*NAlunos*HorasCurso  *  NCursos
+% Income: HCurso1*Vaga1*10 + HCurso2*Vaga2*10 + ... + HCursoN*VagaN*10
 % Outcome: 
-%     Salario Professores: (25  +  30  +  40)*Horas, Horas in 4..16
-%     Salario Funcionarios: 
+%     Salario Professores: (25*HProf1 + 30*HProf2 + 40*HProf3 +.. + X*HProfN), Horas in 4..16
+%     Salario Funcionarios: (2*40*15 + HExtra*25 + HPt*10),   HExtra in 0..MaxHEx, HPt in 0..MaxHPt  (MaxHEx e MaxHPt alteravel)
 
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
+%:- compile('dados1.pl').
+:- compile('dados2.pl').
 
-% Cursos = [Espanhol,Frances,Ingles,Italiano,Alemao,Russo,Portugues]
-%              1        2      3       4       5      6       7         
-
-profs(['Annete','Charles','Boris']).
-curso(1,'Espanhol').
-curso(2,'Frances').
-curso(3,'Ingles').
-curso(4,'Italiano').
-curso(5,'Alemao').
-curso(6,'Russo').
-curso(7,'Portugues').
-
-/*
-curso_prof([1,2,5,7],    %Prof. Annette
-		   [1,3,4,6],    %Prof. Charles
-		   [1,2,4,6,7]). %Prof. Boris
-*/
-
-%prof_curso([[1,2,3],[1,3],[2],[2,3],[1],[2,3],[1,3]]).		   
-indisp_prof_curso([[],[2],[1,3],[1],[2,3],[1],[2]]).
-		   
-preco_prof([25,30,40]).
-
-max_hextra(10).
-max_hpt(10).
-
-% Curso-Vaga
-caso1([1-15,2-15,3-15]).
-caso2([1-13,2-15,3-11,4-14]).
-caso3([1-1,2-1,3-1,4-1,5-1]).
-caso4([1-1,2-1,3-1,4-1,5-1,6-1]).
-
-%IndCurso - lista dos indices dos cursos,  TotalVaga - Somatorio das vagas de tudos cursos
-sep_caso([],IndCurso,IndCurso,0).
-sep_caso([NCurso-Vaga|Resto],Acc,IndCurso,TotalVaga):-
-		sep_caso(Resto,[NCurso|Acc],IndCurso,Acum),
-		TotalVaga is Acum+Vaga.
-
-% verifica se nr de cursos de cada professor está entre 1 e 2
-verifica_nr_cursos_prof(_,0).
-verifica_nr_cursos_prof(NProf,N):- count(N,NProf,#=,Count),Count#=1 #\ Count#=2,
-						N1 is N-1,
-						verifica_nr_cursos_prof(NProf,N1).
-
-aplica_prof_aux([],_).	
-aplica_prof_aux([IndispProf1|T],NProf1):-
-		NProf1 #\= IndispProf1, aplica_prof_aux(T,NProf1).
-												
-aplica_prof(_IndispProfCurso,[],_NProf).
-aplica_prof(IndispProfCurso,[Ind1|Resto],[NProf1|NProfResto]):-
-		nth1(Ind1,IndispProfCurso,IndispProf), aplica_prof_aux(IndispProf,NProf1),
-		aplica_prof(IndispProfCurso,Resto,NProfResto).
-
-horas_por_professor(_,_,_HProf,0).
-horas_por_professor(HCurso,NProf,HProf,NP):-
-		horas_aux(HCurso,NProf,NP,Total),
-		nth1(NP,HProf,Total),
-		NP1 is NP-1,
-		horas_por_professor(HCurso,NProf,HProf,NP1).
-					
-horas_aux([],[],_,0).				
-horas_aux([HCurso1|HResto],[NProf1|NResto],NP,Total):-
-		(NP #= NProf1) #<=> A, horas_aux(HResto,NResto,NP,Acum),
-		Total #= Acum+HCurso1*A.
-		
-print_plano([],[],_,_).				
-print_plano([H|Hs],[P|Ps],ListaProfs,[Ind1|Inds]):-	
-		write(' Curso de '), curso(Ind1,Curso), write(Curso), nl,
-		nth1(P,ListaProfs,NomeP),
-		write('    Professor: '), write(NomeP), nl,
-		write('    Numero Horas Semanais: '), write(H), nl,
-		print_plano(Hs,Ps,ListaProfs,Inds).
-
+solve(Caso):-
+		statistics(walltime,[Start,_]),
+        nl, escola(Caso),
+		statistics(walltime,[End,_]), 
+		Time is End - Start,
+		nl, format('Solutions in ~3d seconds.~n', [Time]),nl,
+        fd_statistics,nl.			
 						
 escola(Caso):-
 		length(Caso,N), length(HCurso,N), length(NProf,N),
-		profs(ListaProfs), length(ListaProfs,NP),length(HProf,NP),
+		profs(ListaProfs), length(ListaProfs,NP),%length(HProf,NP),
 		preco_prof(PrecoProf),
 		domain(HCurso,4,8), % lista com as horas de cada curso (indice)
-		domain(NProf,1,3),  % lista c professor p cada curso (indice)
-		
-		%curso_prof(CursoProf), prof_curso(ProfCurso),
+		domain(NProf,1,NP),  % lista c professor p cada curso (indice)
+
 		indisp_prof_curso(IndispProfCurso),	
-		sep_caso(Caso,[],IndCurso1,TotalVaga), reverse(IndCurso1,IndCurso),
+		sep_caso(Caso,[],IndCurso1,_TotalVaga), reverse(IndCurso1,IndCurso),
 		aplica_prof(IndispProfCurso,IndCurso,NProf),
 		verifica_nr_cursos_prof(NProf,NP),
 		
-		sum(HCurso,#=,TotalHCurso),
-	%	Income #= TotalHCurso*TotalVaga*10, % receita total por semana
 		length(LucroPorCurso,N),
-
 		lucro_por_curso(HCurso,NProf,Caso,PrecoProf,[],LucroPorCurso),
 
 		sum(LucroPorCurso,#=,LucroDosCursos),
-		horas_por_professor(HCurso,NProf,HProf,NP),		
-
+		%horas_por_professor(HCurso,NProf,HProf,NP),		
+		%scalar_product([25,30,40],HProf,#=,CustoProf),
+		%Income#>CustoProf+CustoFunc,
+		
 		max_hextra(MaxHEx),max_hpt(MaxHPt),
 		HExtra in 0..MaxHEx, HPt in 0..MaxHPt,
 		(N*15) #=< 2*40 + HExtra + HPt,
 		CustoFunc #= (2*40*15 + HExtra*25 + HPt*10),
-		
-		%scalar_product([25,30,40],HProf,#=,CustoProf),
-		%Income#>CustoProf+CustoFunc,
+
 		LucroSemanal #= LucroDosCursos - CustoFunc,
-		List=[HExtra,HPt],
 		append(HCurso,NProf,L1),
-		labeling([minimize(CustoFunc)],List),
+		labeling([minimize(CustoFunc)],[HExtra,HPt]),
 		labeling([maximize(LucroSemanal)],L1),
 		
 		write('Maximo Lucro Semanal: '), write(LucroSemanal), nl, nl,
 		write('Plano:'),nl,
 		print_plano(HCurso,NProf,ListaProfs,IndCurso), nl,
 		write(' Horas Extra Necessarias das Funcionarias Administrativas: '), write(HExtra),nl,
-		write(' Horas de Funcionaria Part-time: '), write(HPt),nl.
+		write(' Horas Necessarias de Funcionaria Part-time: '), write(HPt),nl.
+
+
+		
+%IndCurso - lista dos indices dos cursos,  TotalVaga - Somatorio das vagas de tudos cursos
+sep_caso([],IndCurso,IndCurso,0).
+sep_caso([NCurso-Vaga|Resto],Acc,IndCurso,TotalVaga):-
+		sep_caso(Resto,[NCurso|Acc],IndCurso,Acum),
+		TotalVaga is Acum+Vaga.
+
+		
+% verifica se nr de cursos de cada professor está entre 1 e 2
+verifica_nr_cursos_prof(_,0).
+verifica_nr_cursos_prof(NProf,N):-
+		count(N,NProf,#=,Count),Count#=1 #\ Count#=2,
+		N1 is N-1, 
+		verifica_nr_cursos_prof(NProf,N1).
+
+		
+aplica_prof_aux([],_).	
+aplica_prof_aux([IndispProf1|T],NProf1):-
+		NProf1 #\= IndispProf1, aplica_prof_aux(T,NProf1).
+
+		
+aplica_prof(_IndispProfCurso,[],_NProf).
+aplica_prof(IndispProfCurso,[Ind1|Resto],[NProf1|NProfResto]):-
+		nth1(Ind1,IndispProfCurso,IndispProf), 
+		aplica_prof_aux(IndispProf,NProf1),
+		aplica_prof(IndispProfCurso,Resto,NProfResto).
+
+/*		
+horas_por_professor(_,_,_HProf,0).
+horas_por_professor(HCurso,NProf,HProf,NP):-
+		horas_aux(HCurso,NProf,NP,Total),
+		nth1(NP,HProf,Total),
+		NP1 is NP-1,
+		horas_por_professor(HCurso,NProf,HProf,NP1).
+*/
+		
+horas_aux([],[],_,0).				
+horas_aux([HCurso1|HResto],[NProf1|NResto],NP,Total):-
+		(NP #= NProf1) #<=> A, horas_aux(HResto,NResto,NP,Acum),
+		Total #= Acum+HCurso1*A.
+
 		
 lucro_por_curso([],[],[],_,LucroPorCurso,LucroPorCurso).
 lucro_por_curso([HCurso1|HResto],[NProf1|NResto],[_-Vaga|CResto],PrecoProf,Acc,LucroPorCurso):-
@@ -152,14 +126,14 @@ lucro_por_curso([HCurso1|HResto],[NProf1|NResto],[_-Vaga|CResto],PrecoProf,Acc,L
 		L1 #= HCurso1*Vaga*10 - Salario*HCurso1,
 		lucro_por_curso(HResto,NResto,CResto,PrecoProf,[L1|Acc],LucroPorCurso).
 		
-solve(Caso):-
-		statistics(walltime,[Start,_]),
-        nl, escola(Caso),
-		statistics(walltime,[End,_]), 
-		Time is End - Start,
-		nl, format('Solutions in ~3d seconds.~n', [Time]),nl,
-        fd_statistics,nl.	
-	
+		
+print_plano([],[],_,_).				
+print_plano([H|Hs],[P|Ps],ListaProfs,[Ind1|Inds]):-	
+		write(' Curso de '), curso(Ind1,Curso), write(Curso), nl,
+		nth1(P,ListaProfs,NomeP),
+		write('    Professor: '), write(NomeP), nl,
+		write('    Numero Horas Semanais: '), write(H), nl,
+		print_plano(Hs,Ps,ListaProfs,Inds).	
 
 		
 	
---
+++ dados1.pl
@@ -0,0 +1,31 @@
+﻿% Cursos = [Espanhol,Frances,Ingles,Italiano,Alemao,Russo,Portugues]
+%              1        2      3       4       5      6       7         
+
+profs(['Annete','Charles','Boris']).
+curso(1,'Espanhol').
+curso(2,'Frances').
+curso(3,'Ingles').
+curso(4,'Italiano').
+curso(5,'Alemao').
+curso(6,'Russo').
+curso(7,'Portugues').
+
+/*
+curso_prof([1,2,5,7],    %Prof. Annette
+		   [1,3,4,6],    %Prof. Charles
+		   [1,2,4,6,7]). %Prof. Boris
+*/
+
+%prof_curso([[1,2,3],[1,3],[2],[2,3],[1],[2,3],[1,3]]).		   
+indisp_prof_curso([[],[2],[1,3],[1],[2,3],[1],[2]]).
+		   
+preco_prof([25,30,40]).
+
+max_hextra(10).
+max_hpt(10).
+
+% Curso-Vaga
+caso1([1-15,2-15,3-15]).
+caso2([1-13,2-15,3-11,4-14]).
+caso3([1-1,2-1,3-1,4-1,5-1]).
+caso4([1-1,2-1,3-1,4-1,5-1,6-1]).
